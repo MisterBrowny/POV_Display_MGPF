83518be6282d01450babbf743e414add1c04aaa8
MisterBrowny <32779782+MisterBrowny@users.noreply.github.com>
2018-10-21 17:36:05 +0200

    stepper

--- POV_Display_MGPF.ino
+++ POV_Display_MGPF.ino
@@ -1,8 +1,13 @@
 #include <EnableInterrupt.h>
+#include "Stepper.h"
 #include "PololuLedStrip.h"
-#include "TimerOne.h"
 
-#define INPUT_CAPTEUR   4
+const int stepsPerRevolution = 200;  // number of steps per revolution
+
+// initialize the stepper library on pins 2 through 5:
+Stepper myStepper(stepsPerRevolution, 2, 3, 4, 5);
+
+#define INPUT_CAPTEUR   8
 #define OUTPUT_COM      7
 
 PololuLedStrip<OUTPUT_COM> ledStrip;
@@ -10,41 +15,13 @@ PololuLedStrip<OUTPUT_COM> ledStrip;
 #define LED_COUNT 28
 rgb_color colors[LED_COUNT];
 
-
-#define NB_SECTOR       2
-
-// Pour le calcul à l'aide du nb de tours sur un temps donné
-//#define TEMPS_COMPTAGE  10000    // en ms
-//#define CONVERT_us_PAR_SECTOR(v)    (unsigned long) ((float) (TEMPS_COMPTAGE * 1000.0f) / (float) (v * NB_SECTOR))
-//#define CONVERT_TR_PAR_MIN(v)       (float) ((float) v * 1000.0f * 60.0f / TEMPS_COMPTAGE)
-
-// Pour le calcul à partir du temps d'un tour
-#define CONVERT_TR_PAR_MIN(v)       (float) (1000000.0f * 60.0f / v)
-#define CONVERT_us_PAR_SECTOR(v)    (unsigned long) (v / NB_SECTOR)
-
-volatile unsigned char  Count;
-unsigned char   MemoCount;
-unsigned char   NbTours;
-unsigned long   Count_ms;
-unsigned long   Count_us;
-unsigned long   MemoCount_us;
-unsigned long   Time_By_Sector; // en µs
-bool            NewTurn = false;
-bool            Running = false;
-unsigned int    Sector_En_Cours;
+unsigned char   Step;
+unsigned int    Delay_Inter_Step = 1000;    // delay entre chaque step en µs 
 
 // Définition des interruptions
 void Capteur_Interrupt()
 {
-    Timer1.stop();
-    Sector_En_Cours = 0;
-    NewTurn = true;
-    Running = true;
-}
-
-void Sector_Interrupt()
-{
-    if (++ Sector_En_Cours >= NB_SECTOR)    {   Running = false;    }
+    Step = 0;
 }
 
 void setup()
@@ -55,164 +32,56 @@ void setup()
     // Configure outputs
     pinMode(OUTPUT_COM, OUTPUT);
 
-    // Initialise la liaison série à 9600 bauds
+    // Initialise la liaison série à 115200 bauds
     Serial.begin(115200);
 
     // Configure interruptions
     enableInterrupt(INPUT_CAPTEUR, Capteur_Interrupt, FALLING);
 
-    Timer1.initialize(); // set a timer of length xxx microseconds
-    Timer1.attachInterrupt(Sector_Interrupt);
-    Timer1.stop();
-}
-
-void Calcul_Time_By_Sector()
-{
-    Count_us = micros() - MemoCount_us;
-    MemoCount_us = micros();
-        
-    Time_By_Sector = CONVERT_us_PAR_SECTOR(Count_us);
-    
-    Timer1.setPeriod(Time_By_Sector);
-    Timer1.start();
-
-    /*
-    // Optionnel affiche le nombre de tours
-    Serial.print("1 tours en ");
-    Serial.print(Count_us);
-    Serial.println("µs");
-    
-    // Optionnel affiche la vitesse en Tr/min
-    Serial.print("Speed: ");
-    Serial.print(CONVERT_TR_PAR_MIN(Count_us));
-    Serial.println("Tr/min");
-    
-    // Optionnel affiche le temps attribué par secteurs
-    Serial.print("Temps par secteur : ");
-    Serial.println(Time_By_Sector);
-    */   
+    // Initialize le moteur pas à pas
+    myStepper.setSpeed(60);
 }
 
 void loop()
 {   
-    unsigned int i;
+    unsigned char temp = Step % 4;
     rgb_color color;
-          
-    if (NewTurn == true)
+
+    switch (temp)
     {
-        NewTurn = false;
-        Calcul_Time_By_Sector();
+        case 0:
+        {
+            color.red = 0;
+            color.green = 0;
+            color.blue = 0;
+        }
+        break;
+        case 1:
+        {
+            color.red = 255;
+            color.green = 0;
+            color.blue = 0;
+        }
+        break;
+        case 2:
+        {
+            color.red = 0;
+            color.green = 255;
+            color.blue = 0;
+        }
+        break;
+        case 3:
+        {
+            color.red = 0;
+            color.green = 0;
+            color.blue = 255;
+        }
+        break;
     }
+    myStepper.onestep(1);
+
+    if (++ Step >= stepsPerRevolution)  {   Step = 0;   }
     
-    if (    (Running == true)
-        &&  (Sector_En_Cours != Memo_Sector_En_Cours))
-    {
-        i = Sector_En_Cours % 2;
-
-        switch (i)
-        {
-            case 0:
-            {
-                color.red = 0;
-                color.green = 0;
-                color.blue = 0;
-            }
-            break;
-            case 1:
-            {
-                color.red = 255;
-                color.green = 0;
-                color.blue = 0;
-             }
-            break;
-            default:
-            {
-                color.red = 0;
-                color.green = 0;
-                color.blue = 0;
-            }
-            break;
-            }
-        }
-        // Update the colors buffer.
-        for(i = 0; i < LED_COUNT; i++)
-        {
-            colors[i] = color;
-        }
-
-        ledStrip.write(colors, LED_COUNT);
-     }
+    if (Delay_Inter_Step)   {   delayMicroseconds(Delay_Inter_Step);   }
 }
 
-/*
-case 3:
-{
-    color.red = 255;
-    color.green = 255;
-    color.blue = 0;
-}
-break;
-case 4:
-{
-    color.red = 127;
-    color.green = 255;
-    color.blue = 0;
-}
-break;
-case 5:
-{
-    color.red = 0;
-    color.green = 255;
-    color.blue = 0;
-}
-break;
-case 6:
-{
-    color.red = 0;
-    color.green = 255;
-    color.blue = 127;
-}
-break;
-case 7:
-{
-    color.red = 0;
-    color.green = 255;
-    color.blue = 255;
-}
-break;
-case 8:
-{
-    color.red = 0;
-    color.green = 127;
-    color.blue = 255;
-}
-break;
-case 9:
-{
-    color.red = 0;
-    color.green = 0;
-    color.blue = 255;
-}
-break;
-case 10:
-{
-    color.red = 127;
-    color.green = 0;
-    color.blue = 255;
-}
-break;
-case 11:
-{
-    color.red = 255;
-    color.green = 0;
-    color.blue = 255;
-}
-break;
-case 12:
-{
-    color.red = 255;
-    color.green = 0;
-    color.blue = 127;
-}
-break;
-*/
