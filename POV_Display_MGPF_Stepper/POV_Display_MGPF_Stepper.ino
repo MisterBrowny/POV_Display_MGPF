103cfbeb69951a87f58e7f2583c0f60b7d9e2034
MisterBrowny <32779782+MisterBrowny@users.noreply.github.com>
2018-11-02 01:29:53 +0100

    Update POV_Display_MGPF_Stepper/POV_Display_MGPF_Stepper.ino

--- POV_Display_MGPF_Stepper/POV_Display_MGPF_Stepper.ino
+++ POV_Display_MGPF_Stepper/POV_Display_MGPF_Stepper.ino
@@ -7,7 +7,7 @@
 // initialize the stepper library on pins 2 through 5:
 //Stepper stepper(stepsPerRevolution, 2, 3, 4, 5);
 
-unsigned int    Value,
+unsigned int    Value;
 unsigned int    Value1 = 3;
 unsigned int    Value_Max = 2000;
 unsigned int    Value_Min = 300;
