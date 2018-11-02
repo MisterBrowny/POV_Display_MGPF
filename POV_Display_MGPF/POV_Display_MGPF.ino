9bbc4318cd409e18ec832c5960601b3fab7bc96e
MisterBrowny <32779782+MisterBrowny@users.noreply.github.com>
2018-11-02 02:05:39 +0100

    Update POV_Display_MGPF/POV_Display_MGPF.ino

--- POV_Display_MGPF/POV_Display_MGPF.ino
+++ POV_Display_MGPF/POV_Display_MGPF.ino
@@ -1,4 +1,5 @@
 #include <EnableInterrupt.h>
+#include <SPI.h>
 #include "PololuLedStrip.h"
 
 const int stepsPerRevolution = 400;  // number of steps per revolution
@@ -15,13 +16,39 @@ rgb_color colors[LED_COUNT];
 unsigned char   Step, Sector, NbTours, MemoNbTours;
 unsigned int    Delay_Inter_Step;
 unsigned int    Delay_Inter_Step_Max = 2000;    // delay entre chaque step en µs 
-unsigned int    Delay_Inter_Step_Min = 300;    // delay entre chaque step en µs 
+unsigned int    Delay_Inter_Step_Min = 860;    // delay entre chaque step en µs 
 bool            InitPos;
+
+
+#define         NB_LED_DISPLAY      161
+#define         NB_BYTE_PAR_LED     3
+
+#define         SPI_TIME_OUT        500 //µs
+
+unsigned long   SPI_Rcv_Time;
+unsigned char   SPI_led_number, SPI_color;
+rgb_color       SPI_colors[NB_LED_DISPLAY];
+
 
 // Définition des interruptions
 void Capteur_Interrupt()
 {
     InitPos = true;
+    Sector = 0;
+}
+
+ISR (SPI_STC_vect)
+{
+    byte c = SPDR;  // grab byte from SPI Data Register
+  
+    // add to buffer if room
+    if (SPI_color == 0)         {   SPI_colors[SPI_led_number].red = c;     }
+    else if (SPI_color == 1)    {   SPI_colors[SPI_led_number].green = c;   }
+    else                        {   SPI_colors[SPI_led_number].blue = c;    }
+    
+    if (++ SPI_color >= NB_BYTE_PAR_LED)    {   SPI_led_number ++;  }
+
+    SPI_Rcv_Time = micros();
 }
 
 void setup()
@@ -41,6 +68,12 @@ void setup()
     pinMode(MOT_STEPPER, OUTPUT);
 
     Delay_Inter_Step = Delay_Inter_Step_Max;
+
+    // turn on SPI in slave mode
+    SPCR |= bit(SPE);
+
+    // now turn on interrupts
+    SPI.attachInterrupt();    //idem : SPCR |= _BV(SPIE);
 }
 
 void loop()
@@ -117,9 +150,15 @@ void loop()
         
         if (++ Sector >= 100)  
         {
-            Sector =0;
+            Sector = 0;
             NbTours ++;
         }
-    } 
+    }
+
+    if ((micros() - SPI_Rcv_Time) > SPI_TIME_OUT)
+    {
+        SPI_color = 0;
+        SPI_led_number = 0;
+    }
 }
 
