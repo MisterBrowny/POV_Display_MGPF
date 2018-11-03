a1e5b788b8a324341b0220cbefe2cc475922df3e
MisterBrowny <32779782+MisterBrowny@users.noreply.github.com>
2018-11-03 07:46:43 +0100

    Update POV_Display_MGPF/POV_Display_MGPF.ino

--- POV_Display_MGPF/POV_Display_MGPF.ino
+++ POV_Display_MGPF/POV_Display_MGPF.ino
@@ -97,7 +97,7 @@ void loop()
 		memcpy(data, rcv_data, NB_DATAS);
 		for(i = 0; i < 3; i++)
 		{
-			Serial.print(data[i]);
+			Serial.print(rcv_data[i]);
 		}
 		Write = false;
 		bitSet(SPCR, SPIE);
