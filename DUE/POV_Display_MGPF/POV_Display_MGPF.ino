91a4db2d09a70c63042717f655b3bc6988fe898c
MisterBrowny <32779782+MisterBrowny@users.noreply.github.com>
2018-11-08 01:30:26 +0100

    Update DUE/POV_Display_MGPF/POV_Display_MGPF.ino

--- DUE/POV_Display_MGPF/POV_Display_MGPF.ino
+++ DUE/POV_Display_MGPF/POV_Display_MGPF.ino
@@ -38,7 +38,7 @@ bool								Motor_Is_Init = false;
 bool								Motor_Is_Running = false;
 
 // SPI
-#define			NB_LED_DISPLAY		100
+#define			NB_LED_DISPLAY		161
 #define			NB_BYTE_PAR_LED		3
 #define			NB_DATAS			(unsigned int) (NB_LED_DISPLAY * NB_BYTE_PAR_LED)
 #define			SPI_TIME_OUT		5	// ms
@@ -259,6 +259,249 @@ void Test_Led (void)
     {
         colors[i] = color;
     }
+}
+
+void COLOR_Refresh_Test (void)
+{
+	unsigned char    i,j;
+	
+	// Group8 - led [28-26] - Data[130 - 161] - 32 pixels
+	// 4 - 3 - 3 - 3 - 3 - 3 - 3 - 3 - ...
+	for (i = 0; i < 3; i ++)
+	{
+		if (Sector < 4)				{	j = 0;	}
+		else if (Sector < 7)		{	j = 1;	}
+		else if (Sector < 10)		{	j = 2;	}
+		else if (Sector < 13)		{	j = 3;	}
+		else if (Sector < 16)		{	j = 4;	}
+		else if (Sector < 19)		{	j = 5;	}
+		else if (Sector < 22)		{	j = 6;	}
+		else if (Sector < 25)		{	j = 7;	}
+		else if (Sector < 29)		{	j = 8;	}
+		else if (Sector < 32)		{	j = 9;	}
+		else if (Sector < 35)		{	j = 10;	}
+		else if (Sector < 38)		{	j = 11;	}
+		else if (Sector < 41)		{	j = 12;	}
+		else if (Sector < 44)		{	j = 13;	}
+		else if (Sector < 47)		{	j = 14;	}
+		else if (Sector < 50)		{	j = 15;	}
+		else if (Sector < 54)		{	j = 16;	}
+		else if (Sector < 57)		{	j = 17;	}
+		else if (Sector < 60)		{	j = 18;	}
+		else if (Sector < 63)		{	j = 19;	}
+		else if (Sector < 66)		{	j = 20;	}
+		else if (Sector < 69)		{	j = 21;	}
+		else if (Sector < 72)		{	j = 22;	}
+		else if (Sector < 75)		{	j = 23;	}
+		else if (Sector < 79)		{	j = 24;	}
+		else if (Sector < 82)		{	j = 25;	}
+		else if (Sector < 85)		{	j = 26;	}
+		else if (Sector < 88)		{	j = 27;	}
+		else if (Sector < 91)		{	j = 28;	}
+		else if (Sector < 94)		{	j = 29;	}
+		else if (Sector < 97)		{	j = 30;	}
+		else 						{	j = 31;	}
+		
+		colors[i].red = 255;
+		colors[i].green = 0;
+		colors[i].blue = 0;
+	}
+
+	// Group7 - led [25-23] - Data[98 - 129] - 32 pixels
+	// 4 - 3 - 3 - 3 - 3 - 3 - 3 - 3 - ...
+	for (i = 3; i < 6; i ++)
+	{
+		if (Sector < 4)				{	j = 0;	}
+		else if (Sector < 7)		{	j = 1;	}
+		else if (Sector < 10)		{	j = 2;	}
+		else if (Sector < 13)		{	j = 3;	}
+		else if (Sector < 16)		{	j = 4;	}
+		else if (Sector < 19)		{	j = 5;	}
+		else if (Sector < 22)		{	j = 6;	}
+		else if (Sector < 25)		{	j = 7;	}
+		else if (Sector < 29)		{	j = 8;	}
+		else if (Sector < 32)		{	j = 9;	}
+		else if (Sector < 35)		{	j = 10;	}
+		else if (Sector < 38)		{	j = 11;	}
+		else if (Sector < 41)		{	j = 12;	}
+		else if (Sector < 44)		{	j = 13;	}
+		else if (Sector < 47)		{	j = 14;	}
+		else if (Sector < 50)		{	j = 15;	}
+		else if (Sector < 54)		{	j = 16;	}
+		else if (Sector < 57)		{	j = 17;	}
+		else if (Sector < 60)		{	j = 18;	}
+		else if (Sector < 63)		{	j = 19;	}
+		else if (Sector < 66)		{	j = 20;	}
+		else if (Sector < 69)		{	j = 21;	}
+		else if (Sector < 72)		{	j = 22;	}
+		else if (Sector < 75)		{	j = 23;	}
+		else if (Sector < 79)		{	j = 24;	}
+		else if (Sector < 82)		{	j = 25;	}
+		else if (Sector < 85)		{	j = 26;	}
+		else if (Sector < 88)		{	j = 27;	}
+		else if (Sector < 91)		{	j = 28;	}
+		else if (Sector < 94)		{	j = 29;	}
+		else if (Sector < 97)		{	j = 30;	}
+		else 						{	j = 31;	}
+		
+		colors[i].red = 0;
+		colors[i].green = 255;
+		colors[i].blue = 0;
+	}
+
+	// Group6 - led [22-20] - Data[70 - 97] - 28 pixels
+	// 4 - 3 - 4 - 3 - 4 - 3 - 4 - ...
+	for (i = 6; i < 9; i ++)
+	{
+		if (Sector < 4)				{	j = 0;	}
+		else if (Sector < 7)		{	j = 1;	}
+		else if (Sector < 11)		{	j = 2;	}
+		else if (Sector < 14)		{	j = 3;	}
+		else if (Sector < 18)		{	j = 4;	}
+		else if (Sector < 21)		{	j = 5;	}
+		else if (Sector < 25)		{	j = 6;	}
+		else if (Sector < 29)		{	j = 7;	}
+		else if (Sector < 32)		{	j = 8;	}
+		else if (Sector < 36)		{	j = 9;	}
+		else if (Sector < 39)		{	j = 10;	}
+		else if (Sector < 43)		{	j = 11;	}
+		else if (Sector < 46)		{	j = 12;	}
+		else if (Sector < 50)		{	j = 13;	}
+		else if (Sector < 54)		{	j = 14;	}
+		else if (Sector < 57)		{	j = 15;	}
+		else if (Sector < 61)		{	j = 16;	}
+		else if (Sector < 64)		{	j = 17;	}
+		else if (Sector < 68)		{	j = 18;	}
+		else if (Sector < 71)		{	j = 19;	}
+		else if (Sector < 75)		{	j = 20;	}
+		else if (Sector < 79)		{	j = 21;	}
+		else if (Sector < 82)		{	j = 22;	}
+		else if (Sector < 86)		{	j = 23;	}
+		else if (Sector < 89)		{	j = 24;	}
+		else if (Sector < 93)		{	j = 25;	}
+		else if (Sector < 96)		{	j = 26;	}
+		else 						{	j = 27;	}
+		
+		colors[i].red = 0;
+		colors[i].green = 0;
+		colors[i].blue = 255;
+	}
+
+	// Group5 - led [19-16] - Data[46 - 69] - 24 pixels
+	// 5 - 4 - 4 - 4 - 4 - 4 - ...
+	for (i = 9; i < 13; i ++)
+	{
+		if (Sector < 5)				{	j = 0;	}
+		else if (Sector < 9)		{	j = 1;	}
+		else if (Sector < 13)		{	j = 2;	}
+		else if (Sector < 17)		{	j = 3;	}
+		else if (Sector < 21)		{	j = 4;	}
+		else if (Sector < 25)		{	j = 5;	}
+		else if (Sector < 30)		{	j = 6;	}
+		else if (Sector < 34)		{	j = 7;	}
+		else if (Sector < 38)		{	j = 8;	}
+		else if (Sector < 42)		{	j = 9;	}
+		else if (Sector < 46)		{	j = 10;	}
+		else if (Sector < 50)		{	j = 11;	}
+		else if (Sector < 55)		{	j = 12;	}
+		else if (Sector < 59)		{	j = 13;	}
+		else if (Sector < 63)		{	j = 14;	}
+		else if (Sector < 67)		{	j = 15;	}
+		else if (Sector < 71)		{	j = 16;	}
+		else if (Sector < 75)		{	j = 17;	}
+		else if (Sector < 79)		{	j = 18;	}
+		else if (Sector < 83)		{	j = 19;	}
+		else if (Sector < 87)		{	j = 20;	}
+		else if (Sector < 91)		{	j = 21;	}
+		else if (Sector < 96)		{	j = 22;	}
+		else 						{	j = 23;	}
+		
+		colors[i].red = 255;
+		colors[i].green = 255;
+		colors[i].blue = 0;
+	}
+
+	// Group4 - led [15-12] - Data[22 - 45] - 24 pixels
+	// 5 - 4 - 4 - 4 - 4 - 4 - ... 
+	for (i = 13; i < 17; i ++)
+	{
+		if (Sector < 5)				{	j = 0;	}
+		else if (Sector < 9)		{	j = 1;	}
+		else if (Sector < 13)		{	j = 2;	}
+		else if (Sector < 17)		{	j = 3;	}
+		else if (Sector < 21)		{	j = 4;	}
+		else if (Sector < 25)		{	j = 5;	}
+		else if (Sector < 30)		{	j = 6;	}
+		else if (Sector < 34)		{	j = 7;	}
+		else if (Sector < 38)		{	j = 8;	}
+		else if (Sector < 42)		{	j = 9;	}
+		else if (Sector < 46)		{	j = 10;	}
+		else if (Sector < 50)		{	j = 11;	}
+		else if (Sector < 55)		{	j = 12;	}
+		else if (Sector < 59)		{	j = 13;	}
+		else if (Sector < 63)		{	j = 14;	}
+		else if (Sector < 67)		{	j = 15;	}
+		else if (Sector < 71)		{	j = 16;	}
+		else if (Sector < 75)		{	j = 17;	}
+		else if (Sector < 79)		{	j = 18;	}
+		else if (Sector < 83)		{	j = 19;	}
+		else if (Sector < 87)		{	j = 20;	}
+		else if (Sector < 91)		{	j = 21;	}
+		else if (Sector < 96)		{	j = 22;	}
+		else 						{	j = 23;	}
+		
+		colors[i].red = 0;
+		colors[i].green = 255;
+		colors[i].blue = 255;
+	}
+
+	// Group3 - led [11-08] - Data [10 - 21] - 12 pixels
+	// 8 - 9 - 8 - ...
+	for (i = 17; i < 21; i ++)
+	{
+		if (Sector < 8)				{	j = 0;	}
+		else if (Sector < 17)		{	j = 1;	}
+		else if (Sector < 25)		{	j = 2;	}
+		else if (Sector < 33)		{	j = 3;	}
+		else if (Sector < 42)		{	j = 4;	}
+		else if (Sector < 50)		{	j = 5;	}
+		else if (Sector < 58)		{	j = 6;	}
+		else if (Sector < 67)		{	j = 7;	}
+		else if (Sector < 75)		{	j = 8;	}
+		else if (Sector < 83)		{	j = 9;	}
+		else if (Sector < 92)		{	j = 10;	}
+		else 						{	j = 11;	}
+		
+		colors[i].red = 255;
+		colors[i].green = 0;
+		colors[i].blue = 255;
+	}
+
+	// Group2 - led [07-04] - Data [2 - 9] - 8 pixels
+	// 12 - 13 - 12 - ...
+	for (i = 21; i < 25; i ++)
+	{
+		if (Sector < 12)			{	j = 0;	}
+		else if (Sector < 25)		{	j = 1;	}
+		else if (Sector < 37)		{	j = 2;	}
+		else if (Sector < 50)		{	j = 3;	}
+		else if (Sector < 62)		{	j = 4;	}
+		else if (Sector < 75)		{	j = 5;	}
+		else if (Sector < 87)		{	j = 6;	}
+		else 						{	j = 7;	}
+		
+		colors[i].red = 127;
+		colors[i].green = 255;
+		colors[i].blue = 127;
+	}
+
+	// Group1 - led [03-01] - Data [1] - 1 pixel
+	for (i = 25; i < 28; i ++)
+	{
+		colors[i].red = 127;
+		colors[i].green = 0;
+		colors[i].blue = 0;
+	}
 }
 
 void COLOR_Refresh (void)
@@ -508,12 +751,15 @@ void LED_Refresh (void)
 {
 	Sector = Step / 2;
 	
-	if (	(Sector != MemoSector)
-		&&	(Spi0.DataOk == true))
+	//if (	(Sector != MemoSector)
+	//	&&	(Spi0.DataOk == true))
+	if (Sector != MemoSector)
 	{
 		MemoSector = Sector;
 
-		COLOR_Refresh();
+		//COLOR_Refresh();
+		
+		COLOR_Refresh_Test();
 		
 		ledStrip.write(colors, LED_COUNT);
 	}
