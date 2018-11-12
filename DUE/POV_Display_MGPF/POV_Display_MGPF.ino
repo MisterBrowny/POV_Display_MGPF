5e66ffcee4c5c22d91c9ca5cce737553125d4007
MisterBrowny <32779782+MisterBrowny@users.noreply.github.com>
2018-11-12 23:25:38 +0100

    Update DUE/POV_Display_MGPF/POV_Display_MGPF.ino

--- DUE/POV_Display_MGPF/POV_Display_MGPF.ino
+++ DUE/POV_Display_MGPF/POV_Display_MGPF.ino
@@ -155,7 +155,7 @@ void loop()
     }
   }
   */
-  if (    ((Time_us - Refresh_Time) > 1500)
+  /*if (    ((Time_us - Refresh_Time) > 1500)
       &&  (Spi0.Save_Time == false)
       &&  (Spi0.Check_Time_Out == false))
   {
@@ -163,7 +163,7 @@ void loop()
     LED_Refresh();
     SPI_Slave_Stop();
     SPI_Slave_Initialize(SPI_MODE0);
-  }
+  }*/
   SPI_Refresh_Data();
   //LED_Refresh_Test();
   //delayMicroseconds(1500);
